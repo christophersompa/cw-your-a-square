3a10e46dc326fd6fc32047a1f8d335ebb7c186df
Christopher <cgtsompa@gmail.com>
2024-10-13 23:36:07 +0100

    Add project files.

---
+++ your-a-square/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace your_a_square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Given an integral number, determine if it's a square number.
+             */
+
+            Console.WriteLine(IsSquare(4));
+            Console.ReadLine();
+
+        }
+
+        public static bool IsSquare(int n)
+        {
+            return (Math.Sqrt(n) % 1 == 0);
+        }
+    }
+}
